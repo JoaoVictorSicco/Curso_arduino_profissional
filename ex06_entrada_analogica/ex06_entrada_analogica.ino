6d88753d7da6534aa3f9b35e313bc69220a51794
JoaoVictorSicco <victordesicco@gmail.com>
2023-05-27 09:05:39 -0400

    Curso arduino profissional/Aula 3.6 - Entrada analógica/ex06 exemplo entrada analógica usando o potenciometro

--- ex06_entrada_analogica/ex06_entrada_analogica.ino
+++ ex06_entrada_analogica/ex06_entrada_analogica.ino
@@ -8,11 +8,13 @@
 #define debounce_time   450
 #define POT              A0
 
-bool BT1_STATUS = 0;
-bool LEDSTATUS = 0;
+//bool BT1_STATUS = 0;
+//bool LEDSTATUS = 0;
 
-void leitura_botao();
+//void leitura_botao();
 int leitura_potenciometro();            //Função que realiza a leitura de um potenciômetro e retorna um valor inteiro
+
+float converte_tensao(int valor_do_potenciometro, float tensao_maxima);
 
 void setup()
 {
@@ -24,15 +26,24 @@ void setup()
 
 void loop()
 {
-  static long long ElapsedTime = 0;
-  leitura_botao();
-  digitalWrite(LED1, BT1_STATUS);
+  static long long ElapsedTime = 0;     // Variável para comparação do tempo millis
+  int valor_potenciometro = 0;
+  float tensao = 0;
+  //leitura_botao();
+  //digitalWrite(LED1, BT1_STATUS);
   if(ElapsedTime +250<millis())
   {
     ElapsedTime = millis();
 
-    Serial.print("LED1 STATUS:");
-    Serial.println(POT);
+//    Serial.print("LED1 STATUS:");
+//    Serial.println(POT);
+
+    Serial.print("Status do potenciometro: ");
+    // Leitura do potenciometro
+    valor_potenciometro = leitura_potenciometro(); //Função de leitura
+    tensao = converte_tensao(valor_potenciometro,5); //Conversão para tensão
+    Serial.print(tensao);
+    Serial.println();
     
   }
 
@@ -40,37 +51,44 @@ void loop()
 }
 
 
-void leitura_botao()
-{
-  static bool BT1_FLAG = 0;             // Variável de sinalização pra saber quando o botão comutou
-  static long long BT1_DEBOUNCE = 0;    // Variável para o uso do millis
-
-  // Verifica se o botão foi pressionado e muda o status de BT1_FLAG para 1 dentro do if
-  if (!digitalRead(BT1) && !BT1_FLAG)
-  {
-    BT1_FLAG = 1;
-    BT1_DEBOUNCE = millis();            // Registra o tempo que isso aconteceu
-  }
-
-  // Verifica se o botão continua pressionado, se BT1_FLAG continua em HIGH e se o tempo de debounce já ocorreu
-  if (!digitalRead() && BT1_FLAG && (BT1_DEBOUNCE + debounce_time < millis()))
-  {
-    BT1_DEBOUNCE = millis();
-    bt1_status = 1;
-
-  }
-
-  if (digitalRead() && BT1_FLAG && (BT1_DEBOUNCE + debounce_time < millis()))
-  {
-    BT1_FLAG = 0;
-    BT1_STATUS = 0;
-
-  }
-}
+//void leitura_botao()
+//{
+//  static bool BT1_FLAG = 0;             // Variável de sinalização pra saber quando o botão comutou
+//  static long long BT1_DEBOUNCE = 0;    // Variável para o uso do millis
+//
+//  // Verifica se o botão foi pressionado e muda o status de BT1_FLAG para 1 dentro do if
+//  if (!digitalRead(BT1) && !BT1_FLAG)
+//  {
+//    BT1_FLAG = 1;
+//    BT1_DEBOUNCE = millis();            // Registra o tempo que isso aconteceu
+//  }
+//
+//  // Verifica se o botão continua pressionado, se BT1_FLAG continua em HIGH e se o tempo de debounce já ocorreu
+//  if (!digitalRead() && BT1_FLAG && (BT1_DEBOUNCE + debounce_time < millis()))
+//  {
+//    BT1_DEBOUNCE = millis();
+//    bt1_status = 1;
+//
+//  }
+//
+//  if (digitalRead() && BT1_FLAG && (BT1_DEBOUNCE + debounce_time < millis()))
+//  {
+//    BT1_FLAG = 0;
+//    BT1_STATUS = 0;
+//
+//  }
+//}
 
 int leitura_potenciometro()
 {
   int valor = 0;
   valor = analogRead(A0);
   return valor;
+}
+
+float converte_tensao(int valor_do_potenciometro, float tensao_maxima)
+{
+  float escala = (float)tensao_maxima/(float)1024;
+  return valor_do_potenciometro*escala;
+  
 }
