526a935e3411297e7299300e53c04ece30a26a46
JoaoVictorSicco <victordesicco@gmail.com>
2023-06-05 10:15:06 -0400

    Adição do novo módulo

---
+++ ex12_dht11_sensor_temperatura_umidade/ex12_dht11_sensor_temperatura_umidade.ino
@@ -0,0 +1,20 @@
+// Curso arduino profissional WRKITS
+// Aula 6.1 - DHT11//22 Sensor de temperatura e umidade
+// Exemplo sensor de temperatura
+
+
+void setup()
+{
+  
+  
+}
+
+void loop()
+{
+   
+  
+}
+ 
+
+
+  
